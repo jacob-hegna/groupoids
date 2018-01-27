6425e4df2e561063c13fad54760f99d24a50ab0e
Jacob Hegna <jacobhegna@gmail.com>
2018-01-26 18:40:05 -0600

    Add command line options to help with experiments

--- main.cpp
+++ main.cpp
@@ -1,53 +1,111 @@
 #include <iostream>
+#include <unistd.h>
+#include <thread>
 
 #include "algorithm.h"
 #include "groupoid.h"
+#include "test.h"
 
-bool test_lorries_rnd_gpd() {
-    std::vector<std::vector<uint>> lorries_table = {{
-        {3, 4, 1, 0, 4},
-        {2, 3, 0, 1, 2},
-        {0, 2, 4, 3, 1},
-        {1, 0, 3, 4, 1},
-        {1, 1, 0, 3, 2}
-    }};
+void search_nsr(uint size, uint iterations, bool verbose) {
+    uint separating = 0;
+    uint i = 0;
+    while(i < iterations || iterations == 0) {
+        auto groupoid = Groupoid::from_random(size);
+        auto algo_tb  = std::make_shared<Groupoid>(size);
+        bool nsr = Algorithm::check_sr(groupoid, algo_tb);
+        
+        if(nsr) {
+            ++separating;
 
-    std::vector<std::vector<uint>> lorries_table_algo = {{
-        {1, 2, 3, 3, 2},
-        {0, 1, 3, 3, 2},
-        {0, 0, 1, 2, 2},
-        {0, 0, 0, 1, 2},
-        {0, 0, 0, 0, 1}
-    }};
+            std::cout << "(" << separating << "/" << i << ") - "
+                      << (double)separating/(double)i << "%"
+                      << std::endl;
 
-    auto groupoid  = std::make_shared<Groupoid>(lorries_table);
-    auto expect_tb = std::make_shared<Groupoid>(lorries_table_algo);
-    auto algo_tb   = std::make_shared<Groupoid>(groupoid->size());
+            std::cout << groupoid->to_str() << std::endl;
 
-    Algorithm::check_sr(groupoid, algo_tb);
+            if(verbose) {
+                std::cout << std::endl << algo_tb->to_str() << std::endl;
+            }
 
-    return *algo_tb == *expect_tb;
+            std::cout << std::endl;
+        }
+
+        ++i;
+    }
 }
 
 int main(int argc, char* argv[]) {
-    // make sure everything still works
-    if(!test_lorries_rnd_gpd()) {
-        std::cout << "[TEST FAILURE] [Lorrie's random groupoid]" << std::endl;
-        std::cout << "The random groupoid below might not be analyzed correctly!" << std::endl;
+    // default values
+    uint size       = 4;
+    uint iterations = 1000;
+    uint threads    = 1;
+    bool verbose    = false;
+    bool help_shown = false;
+    auto help_short = "Usage: " + std::string(argv[0]) + " [-hvn:i:t:] [test|random|nsr]";
+    auto help_long  = std::string("\t-h print this message\n")
+                    + std::string("\t-v verbose, show algorithm details\n")
+                    + std::string("\t-n size of the groupoids to consider\n")
+                    + std::string("\t-i number of iterations to run (if 0, continue indefinitely)")
+                    + std::string("\t-t threads to use");
+
+    char c;
+    while((c = getopt(argc, argv, "hvn:i:t:")) != -1) {
+        switch(c) {
+            case 'h':
+                help_shown = true;
+                std::cout << help_short << std::endl
+                          << help_long  << std::endl;
+                break;
+            case 'v':
+                verbose = true;
+                break;
+            case 'n':
+                size       = atoi(optarg);
+                break;
+            case 'i':
+                iterations = atoi(optarg);
+                break;
+            case 't':
+                threads    = atoi(optarg);
+                break;
+        }
     }
 
-    // spit out a random groupoid with its table
-    auto groupoid = Groupoid::from_random(5);
-    auto algo_tb  = std::make_shared<Groupoid>(5);
+    if(optind >= argc && !help_shown) {
+        std::cout << help_short << std::endl;
+        return 1;
+    }
 
-    Algorithm::check_sr(groupoid, algo_tb);
+    if(strcmp(argv[optind], "test") == 0) {
+        std::cout << "[test] [Lorrie's random groupoid] ";
+        if(!Test::lorries_rnd_gpd()) {
+            std::cout << "[FAILURE]" << std::endl;
+        } else {
+            std::cout << "[PASS]" << std::endl;
+        }
 
-    std::cout << "Groupoid: "
-              << std::endl
-              << groupoid->to_str()
-              << std::endl
-              << "----------"
-              << std::endl
-              << algo_tb->to_str()
-              << std::endl;
+        std::cout << "[test] [Z5] ";
+        if(!Test::lorries_rnd_gpd()) {
+            std::cout << "[FAILURE]" << std::endl;
+        } else {
+            std::cout << "[PASS]" << std::endl;
+        }
+    } else if(strcmp(argv[optind], "random") == 0) {
+        auto groupoid = Groupoid::from_random(size);
+        auto algo_tb  = std::make_shared<Groupoid>(size);
+
+        Algorithm::check_sr(groupoid, algo_tb);
+
+        std::cout << groupoid->to_str() << std::endl
+                  << std::endl
+                  << algo_tb->to_str()  << std::endl;
+    } else if(strcmp(argv[optind], "nsr") == 0) {
+        std::vector<std::thread> thread_pool;
+        for(int i = 0; i < threads; ++i) {
+            thread_pool.emplace_back(&search_nsr, size, iterations, verbose);
+        }
+        for(auto& thread : thread_pool) {
+            thread.join();
+        }
+    }
 }
