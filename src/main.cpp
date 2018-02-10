120249e59762e7d95bb421f366db019fec465d05
Jacob Hegna <jacobhegna@gmail.com>
2018-02-09 19:11:38 -0600

    Make the command line options sensible

--- src/main.cpp
+++ src/main.cpp
@@ -38,18 +38,12 @@ void search_nsr(uint size, uint iterations, bool verbose) {
 int main(int argc, char* argv[]) {
     // default values
     uint size            = 4;
-    uint iterations      = 1000;
-    uint threads         = 1;
-    bool verbose         = false;
-    std::string filename = "graph.dot";
+    uint iterations      = 1;
     bool help_shown      = false;
     auto help_short      = "Usage: " + std::string(argv[0]) + " [-hvn:i:t:o:] [test|random|nsr-compute|nsr-graph]";
     auto help_long       = std::string("\t-h print this message\n")
-                         + std::string("\t-v verbose, show algorithm details\n")
                          + std::string("\t-n size of the groupoids to consider\n")
-                         + std::string("\t-i number of iterations to run (if 0, continue indefinitely)\n")
-                         + std::string("\t-t threads to use\n")
-                         + std::string("\t-o output filename");
+                         + std::string("\t-i number of iterations to run (if 0, continue indefinitely)\n");
 
     char c;
     while((c = getopt(argc, argv, "hvn:i:t:o:")) != -1) {
@@ -59,17 +53,11 @@ int main(int argc, char* argv[]) {
                 std::cout << help_short << std::endl
                           << help_long  << std::endl;
                 break;
-            case 'v':
-                verbose = true;
-                break;
             case 'n':
                 size       = atoi(optarg);
                 break;
             case 'i':
                 iterations = atoi(optarg);
-                break;
-            case 't':
-                threads    = atoi(optarg);
                 break;
         }
     }
@@ -80,53 +68,42 @@ int main(int argc, char* argv[]) {
         return 1;
     }
 
-    if(strcmp(argv[optind], "test") == 0) {
+    // I'm mixing up my curly brace style here, but otherwise this is unreadable
+    if(strcmp(argv[optind], "test") == 0)
+    {
         Test::run_all_tests();
-    } else if(strcmp(argv[optind], "random") == 0) {
-        auto groupoid = Groupoid::from_random(size);
-        auto algo_tb  = std::make_shared<Groupoid>(size);
+    }
+    else if(strcmp(argv[optind], "generate-random") == 0)
+    {
+        for(int i = 0; i < iterations; ++i) {
+            auto groupoid = Groupoid::from_random(size);
+            std::cout << groupoid->to_str() << std::endl;
 
-        Algorithm::check_sr(groupoid, algo_tb);
-
-        std::cout << groupoid->to_str() << std::endl
-                  << std::endl
-                  << algo_tb->to_str()  << std::endl;
-    } else if(strcmp(argv[optind], "nsr-compute") == 0) {
-        std::vector<std::thread> thread_pool;
-        for(int i = 0; i < threads; ++i) {
-            thread_pool.emplace_back(&search_nsr, size, iterations / threads, verbose);
+            if(i != iterations) {
+                std::cout << std::endl;
+            }
         }
-        for(auto& thread : thread_pool) {
-            thread.join();
-        }
-    } else if(strcmp(argv[optind], "nsr-graph") == 0) {
-        auto asdf = parse_groupoid_list("test.txt");
-        std::cout << asdf.size() << std::endl
-                  << asdf.at(0)->to_str() << std::endl;
+    }
+    else if(strcmp(argv[optind], "generate-sr") == 0)
+    {
+        int i = 0;
+        while(i < iterations) {
+            auto groupoid = Groupoid::from_random(size);
+            if(Algorithm::check_sr(groupoid)) {
+                std::cout << groupoid->to_str() << std::endl;
+                ++i;
 
-        /*
-        std::vector<std::vector<uint>> groupoid_table = {{
-            {2, 3, 2, 3},
-            {3, 2, 3, 2},
-            {0, 1, 0, 1},
-            {1, 0, 1, 0}
-        }};
-        auto groupoid = std::make_shared<Groupoid>(groupoid_table);
-        */
-        
-        //auto groupoid = Groupoid::from_random(6);
-        //auto asdf = std::make_shared<Groupoid>(6);
-        /*
-        while(!Algorithm::check_sr(groupoid, asdf)) {
-            groupoid = Groupoid::from_random(3);
-            asdf = std::make_shared<Groupoid>(3);
-        }*/
-/*
-        Algorithm::check_sr(groupoid, asdf);
-        Algorithm::sr_graph(groupoid, filename);
-            
-        std::cout << groupoid->to_str() << std::endl
-                  << std::endl
-                  << asdf->to_str() << std::endl;*/
+                if(i != iterations) {
+                    std::cout << std::endl;
+                }
+            }
+        }
+    }
+    else if(strcmp(argv[optind], "nsr-graph") == 0)
+    {
+
+        auto gpd = parse_groupoid(std::cin);
+        std::cout << gpd->to_str() << std::endl;
+        Algorithm::sr_graph(gpd, std::cout, true);
     }
 }
